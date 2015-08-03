bd73bf2cba628c8d809e05102e2e1e6cddaa2723
SopaXorzTaker <yellowmark@yandex.ru>
2015-08-03 12:45:51 +0300

    Dynamic world rendering - speedup!

--- main.py
+++ main.py
@@ -231,9 +231,11 @@ def main_loop():
                     screenshot()
 
         map_display.fill(Color(154, 198, 255, 0))
-        for x in xrange(MAP_X):
-            for y in xrange(MAP_Y):
-                map_display.blit(block.BLOCK_TEXTURES[wrl.level[x][y]], (x * 32, y * 32))
+        py, px = wrl.player.coords
+        for x in xrange(px - 26, px + 26):
+            for y in xrange(py - 20, py + 20):
+                if x in xrange(MAP_X) and y in xrange(MAP_Y):
+                    map_display.blit(block.BLOCK_TEXTURES[wrl.level[x][y]], (x * 32, y * 32))
 
         debug_text = "Coords: %d, %d   %d fps, block: " % (
                 wrl.player.coords[0], wrl.player.coords[1], clk.get_fps()) + "**%d, %d**" % (xboundmax, yboundmax) + " player_health: %d " % (player_health)
