c5f0b529a45c88f96c1d648dba8ca0916f472c44
viktorahlstroem <sno.kattunge@gmail.com>
2015-08-03 05:59:10 -0400

    modify main.py

--- main.py
+++ main.py
@@ -32,14 +32,13 @@ sy = 0
 
 config.load()
 
-
 def screenshot():
-
     filename = "2dexp-%s.png" % str(datetime.datetime.now()).replace(":", "-")
     f = open(filename, "w")  # Create the file
     f.close()
     pygame.image.save(display, filename)
     print "Saved screenshot"
+
 
 def load(filename):
     import cPickle, gzip
@@ -85,7 +84,7 @@ def game_over():
 
 
 def main_loop():
-    global display, start_time, fall_delay, map_display, block_above, block_under, block_index, wrl, xs, ys,\
+    global display, start_time, fall_delay, map_display, block_above, block_under, block_index, wrl, xs, ys, \
         just_started
     ys = 0
     xs = 0
@@ -93,29 +92,33 @@ def main_loop():
     start_time = 0
     clk = pygame.time.Clock()
     if wrl.player.coords[1] < 12:
-        xboundmax=(wrl.player.coords[1] + 12)
+        xboundmax = (wrl.player.coords[1] + 12)
     else:
-        xboundmax=((wrl.player.coords[1]-(wrl.player.coords[1]*2))+12)
-    xboundmin=0
+        xboundmax = ((wrl.player.coords[1] - (wrl.player.coords[1] * 2)) + 12)
+    xboundmin = 0
     if wrl.player.coords[1] < 8:
-        yboundmax=(wrl.player.coords[0])
+        yboundmax = (wrl.player.coords[0])
     else:
-        yboundmax = ((wrl.player.coords[0] - (wrl.player.coords[0] * 2)) + 8)
-    yboundmin = 0
+<<<<<<< HEAD
         yboundmax=((wrl.player.coords[0]-(wrl.player.coords[0]*2))+8)
     yboundmin=0
+    player_health=20
+=======
+        yboundmax = ((wrl.player.coords[0] - (wrl.player.coords[0] * 2)) + 8)
+    yboundmin = 0
+>>>>>>> 2c38872fade8afcd9e118695d84ccac98ab46604
     while True:
-        has_displayed=0
+        has_displayed = 0
         if wrl.player.coords[1] < 12:
-            xboundmax=(wrl.player.coords[1] + 12)
+            xboundmax = (wrl.player.coords[1] + 12)
         else:
-            xboundmax=((wrl.player.coords[1]-(wrl.player.coords[1]*2))+12)
-        xboundmin=0
+            xboundmax = ((wrl.player.coords[1] - (wrl.player.coords[1] * 2)) + 12)
+        xboundmin = 0
         if wrl.player.coords[0] < 8:
-             yboundmax=(wrl.player.coords[0] + 8)
+            yboundmax = (wrl.player.coords[0] + 8)
         else:
-            yboundmax=((wrl.player.coords[0]-(wrl.player.coords[0]*2))+8)
-        yboundmin=0
+            yboundmax = ((wrl.player.coords[0] - (wrl.player.coords[0] * 2)) + 8)
+        yboundmin = 0
 
         clk.tick(20)
         if pygame.time.get_ticks() - start_time >= 50:
@@ -154,7 +157,8 @@ def main_loop():
                     yboundmax -= 1
                     if yboundmin != 0:
                         yboundmin -= 1
-                    if not wrl.level[wrl.player.coords[1]][wrl.player.coords[0]] in block.BLOCK_NONSOLID and not keys[K_LSHIFT]:
+                    if not wrl.level[wrl.player.coords[1]][wrl.player.coords[0]] in block.BLOCK_NONSOLID and not keys[
+                        K_LSHIFT]:
                         wrl.player.coords = prev_pos
                     if keys[K_LSHIFT]:
                         bx, by = wrl.player.coords[1], wrl.player.coords[0]
@@ -175,13 +179,13 @@ def main_loop():
                         bx, by = wrl.player.coords[1], wrl.player.coords[0]
                         wrl.destroy_block(bx, by)
                     wrl.player.set_walk(0)
-                        # D KEY - RIGHT
+                    # D KEY - RIGHT
                 elif event.key == K_d and wrl.player.coords[1] in range(0, MAP_Y - 1):
                     wrl.player.falling = False
                     fall_delay = 0
                     wrl.player.coords[1] += 1
                     xboundmax -= 1
-                    if xboundmax <= -24: 
+                    if xboundmax <= -24:
                         xboundmin -= 1
                     if not wrl.level[wrl.player.coords[1]][wrl.player.coords[0]] in block.BLOCK_NONSOLID and not keys[
                         K_LSHIFT]:
@@ -190,7 +194,7 @@ def main_loop():
                         bx, by = wrl.player.coords[1], wrl.player.coords[0]
                         wrl.destroy_block(bx, by)
                     wrl.player.set_walk(1)
-                        # Z KEY - PLACE BLOCK
+                    # Z KEY - PLACE BLOCK
                 elif event.key == K_z:
                     # print "Debug: placing block at %d %d, previous was %d" % (px, py, wrl.level[px][py])
                     if (wrl.player.inventory[
@@ -210,20 +214,20 @@ def main_loop():
                     wrl.player.current_block = (wrl.player.current_block + 1) % len(block.BLOCK_INVENTORY)
                     # ESC KEY - RESET
                 elif event.key == K_ESCAPE:
-                    xboundmax, xboundmin, yboundmax, yboundmin = 0, 0, 0, 0
+                    xboundmax, xboundmin, yboundmax, yboundmin = 0, 0, -10, 0
                     wrl.new_world(MAP_X, MAP_Y)
-                      # F1 KEY - GODMODE
+                    # F1 KEY - GODMODE
                 elif event.key == K_F1:
                     wrl.player.god_mode = not wrl.player.god_mode
                     # E KEY - EXPLODE
                 elif event.key == K_e and wrl.player.god_mode:
                     wrl.explode(px, py, 5, False)
                     # N KEY - DESPAWN ENTITY
-                elif event.key == K_n: #and wrl.player.god_mode:
+                elif event.key == K_n:  # and wrl.player.god_mode:
                     if len(wrl.entities) > 1:
                         wrl.remove_entity(len(wrl.entities) - 1)  # last
                         # M KEY - SPAWN ENTITY
-                elif event.key == K_m: #and wrl.player.god_mode:
+                elif event.key == K_m:  # and wrl.player.god_mode:
                     ent = PlayerEntity(bounding_box=(0, 0, MAP_X, MAP_Y), name="Testificate")
                     wrl.spawn_entity(ent)
                     ent.coords = wrl.player.coords
@@ -232,34 +236,40 @@ def main_loop():
                     screenshot()
 
         map_display.fill(Color(154, 198, 255, 0))
-        for x in range(MAP_X):
-            for y in range(MAP_Y):
+        for x in xrange(MAP_X):
+            for y in xrange(MAP_Y):
                 map_display.blit(block.BLOCK_TEXTURES[wrl.level[x][y]], (x * 32, y * 32))
 
         debug_text = "Coords: %d, %d   %d fps, block: " % (
-            wrl.player.coords[0], wrl.player.coords[1], clk.get_fps()) + "**%d, %d**" % (xboundmax, yboundmax)
+                wrl.player.coords[0], wrl.player.coords[1], clk.get_fps()) + "**%d, %d**" % (xboundmax, yboundmax) + " player_health: %d " % (player_health)
         inventory_text = (" x %d" % wrl.player.inventory.get(wrl.player.current_block,
                                                              -1)) + " " + block.BLOCK_NAMES.get(
-             block.BLOCK_INVENTORY[wrl.player.current_block], "unknown")
+            block.BLOCK_INVENTORY[wrl.player.current_block], "unknown")
         debug_label = font.render(debug_text, True, COLORS['white'], COLORS['black'])
         inventory_label = font.render(inventory_text, True, COLORS['white'], COLORS['black'])
         display.blit(debug_label, (0, 0))
         display.blit(block.BLOCK_TEXTURES[block.BLOCK_INVENTORY[wrl.player.current_block]], (0, 25 * TILESIZE + 5))
         display.blit(inventory_label, (32, MAP_Y * TILESIZE + 5))
         if block_under in block.BLOCK_DEADLY and not wrl.player.god_mode:
-            game_over()
+            player_health -= 0.20
+            if player_health <= 0:
+                game_over()
+        if player_health <= 20.0:
+            player_health=player_health+0.05
         for ent in wrl.entities:
             ent.render(map_display, TILESIZE, TILESIZE)
         pygame.display.update()
         display.fill(0)
         if xboundmax <= 0 and xboundmax <= -12:
-            fx=(xboundmax*32)
-            has_displayed=1 
-            if xboundmax < -23: fx = -736
-        else: fx = 0
+            fx = (xboundmax * 32)
+            if xboundmax < -23:
+                fx = -736
+        else:
+            fx = 0
         if yboundmax <= 0 and yboundmax <= -8:
-            fy=(yboundmax*32)
-        else: fy = 0
+            fy = (yboundmax * 32)
+        else:
+            fy = 0
         display.blit(map_display, (fx, fy))
         display.fill(0x101010, (0, 600 - 48, 800, 600))
         display.blit(block.BLOCK_TEXTURES[block.BLOCK_INVENTORY[wrl.player.current_block]], (8, 600 - 40))
